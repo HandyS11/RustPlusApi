29940459fe7f468e63e3334e8308382227367aca
HandyS11 <valentin-clergue@orange.fr>
2024-03-31 01:39:15 +0100

    test app

---
+++ FcmListener/Program.cs
@@ -0,0 +1,33 @@
+﻿using RustPlusApi.Fcm;
+using RustPlusApi.Fcm.Data;
+
+var credentials = new Credentials
+{
+    Keys = new Keys
+    {
+        PrivateKey = "",
+        PublicKey = "",
+        AuthSecret = "",
+    },
+    Fcm = new FcmCredentials
+    {
+        Token = "",
+        PushSet = "",
+    },
+    Gcm = new GcmCredentials
+    {
+        Token = "",
+        AndroidId = 0,
+        SecurityToken = 0,
+        AppId = "",
+    }
+};
+
+var listener = new FcmListener(credentials, new List<string>());
+
+listener.NotificationReceived += (_, message) =>
+{
+    Console.WriteLine(message);
+};
+
+await listener.ConnectAsync();
