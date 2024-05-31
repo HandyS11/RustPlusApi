7a9760e2d7293c2d0120022d917db433ff7a65e3
Clergue Valentin <valentin-clergue@orange.fr>
2024-05-31 21:53:15 +0200

    Merge feature/addLegacyRequests into develop (#12)
    add getNexusAuth & setClanMotd

---
+++ RustPlusApi/Examples/Legacy/GetNexusAuthLegacy/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+using RustPlusApi;
+
+using static __Constants.ExamplesConst;
+
+var rustPlus = new RustPlusLegacy(Ip, Port, PlayerId, PlayerToken);
+const string apiKey = "";
+
+await rustPlus.ConnectAsync();
+
+// This method is not fully integrated in Rust, so it will not work until the Nexus update is released.
+var message = await rustPlus.GetNexusAuthLegacyAsync(apiKey);
+Console.WriteLine($"Infos:\n{JsonConvert.SerializeObject(message, JsonSettings)}");
+
+await rustPlus.DisconnectAsync();
